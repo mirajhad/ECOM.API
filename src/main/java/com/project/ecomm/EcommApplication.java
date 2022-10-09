c7398930b8e3c9fbd41400c8cf484eaf9f501fc1
miraj <miraj.ansari@sysfore.com>
2022-10-09 19:16:24 +0530

    added hello Miraj app

--- src/main/java/com/project/ecomm/EcommApplication.java
+++ src/main/java/com/project/ecomm/EcommApplication.java
@@ -2,7 +2,10 @@ package com.project.ecomm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EcommApplication {
 
@@ -10,4 +13,8 @@ public class EcommApplication {
 		SpringApplication.run(EcommApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "hello Miraj";
+	}
 }
